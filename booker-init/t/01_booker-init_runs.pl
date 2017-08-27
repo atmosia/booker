2a36d3100d4078e22b8713a70b6d327a5ba6197c
mikeyhc <mikeyhc@atmosia.net>
2017-08-27 19:35:39 +0200

    increase test coverage

--- booker-init/t/01_booker-init_runs.pl
+++ booker-init/t/01_booker-init_runs.pl
@@ -2,16 +2,25 @@
 
 use strict;
 
+sub pass {
+    $_[0]
+}
+
+sub fail {
+    !$_[0]
+}
+
 my @cmds = (
-    ["./booker-init"],
-    ["./booker-init", "-d", "test"],
+    [["./booker-init"], \&pass],
+    [["./booker-init", "-d", "test"], \&pass],
+    [["./booker-init", "-d"], \&fail],
 );
 
 my $exit = 0;
 for my $cmd (@cmds) {
-    printf("running `@$cmd'\n");
-    if (system(@$cmd)) {
-        printf(STDERR "failed to run $cmd\n");
+    printf("running `@{$cmd->[0]}'\n");
+    if ($cmd->[1](system(@{$cmd->[0]}))) {
+        printf(STDERR "failed to run @{$cmd->[0]}\n");
         $exit = 1;
     }
 }
