1bf762faad9eafe5933cd09150c5a08b2f8e1303
mikeyhc <mikeyhc@atmosia.net>
2017-08-31 13:07:24 +0200

    test that tables are created

--- init/t/01_init_runs.pl
+++ init/t/01_init_runs.pl
@@ -4,6 +4,7 @@ use v5.010;
 use strict;
 use warnings;
 
+use DBI;
 use File::Path qw(rmtree);
 
 sub pass {
@@ -19,11 +20,32 @@ sub clean_dir {
     sub { rmtree($tree) }
 }
 
+sub table_exists_sub {
+    my ($file, $name) = @_;
+    sub {
+        my $dbh = DBI->connect("dbi:SQLite:dbname=$file/db.sqlite3", "", "");
+        my $stmt = $dbh->prepare("SELECT COUNT(*) FROM $name");
+        unless ($stmt) {
+            return 0;
+        }
+        $stmt->execute();
+        my $row = $stmt->fetchrow_arrayref();
+        return $row->[0] == 0;
+    }
+}
+
 sub test_files {
     my $path = $_[0];
     [["create directory", sub { -d "$path" }],
      ["create config", sub { -f "$path/config.ini" }],
      ["create db", sub { -f "$path/db.sqlite3" }],
+    ]
+}
+
+sub test_db {
+    my $path = $_[0];
+    [["open session table", table_exists_sub($path, "session")],
+     ["open session_user table", table_exists_sub($path, "session_user")]
     ]
 }
 
@@ -33,12 +55,16 @@ my $HOME = $ENV{HOME};
 my @cmds = (
     { cmd       => ["./booker-init"],
       verify    => \&pass,
-      tests     => test_files("$HOME/.booker"),
+      tests     => [@{test_files("$HOME/.booker")},
+                    @{test_db("$HOME/.booker")},
+                   ],
       clean     => clean_dir("$HOME/.booker"),
     },
     { cmd       => ["./booker-init", "-d", "test-path"],
       verify    => \&pass,
-      tests     => test_files("test-path"),
+      tests     => [@{test_files("test-path")},
+                    @{test_db("test-path")},
+                   ],
       clean     => clean_dir("test-path"),
     },
     { cmd       => ["./booker-init", "-d"],
