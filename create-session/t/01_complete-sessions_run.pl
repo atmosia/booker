2e7fdd25113e141fd2584a6b1758533cda7d78f3
mikeyhc <mikeyhc@atmosia.net>
2017-08-31 19:44:33 +0200

    check user insert and duplicate session

--- create-session/t/01_complete-sessions_run.pl
+++ create-session/t/01_complete-sessions_run.pl
@@ -11,17 +11,47 @@ my $HOME = $ENV{HOME};
 
 my $cd = getcwd;
 my $exit = 0;
+
+# test default session
 chdir("../init");
 system("./booker-init");
 chdir($cd);
 $exit++ if system("./booker-create-session");
 rmtree("$HOME/.booker");
 
+# test special session
 chdir("../init");
 system("./booker-init", "-d", "../create-session/test-path");
 chdir($cd);
 $exit++ if system("./booker-create-session", "-d", "test-path");
 rmtree("test-path");
+
+# test duplicate session
+chdir("../init");
+system("./booker-init");
+chdir($cd);
+$exit++ if system("./booker-create-session");
+$exit++ unless system("./booker-create-session");
+rmtree("$HOME/.booker");
+
+# test users
+chdir("../init");
+system("./booker-init");
+chdir($cd);
+$exit++ if system("./booker-create-session", "-u", "user");
+rmtree("$HOME/.booker");
+
+# test max users
+my @users;
+for my $i (0..10) {
+    push @users, "-u";
+    push @users, "user$i";
+}
+chdir("../init");
+system("./booker-init");
+chdir($cd);
+$exit++ unless system("./booker-create-session", @users);
+rmtree("$HOME/.booker");
 
 $exit++ unless system("./booker-create-session", "-d");
 $exit++ unless system("./booker-create-session", "-d", "");
